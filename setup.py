869a5c1987dd99e29edc0b9976a465fbac4f3411
Hadrien Mary <hadrien.mary@gmail.com>
2017-10-25 20:00:09 -0400

    more fix

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(author_email='hadrien.mary@gmail.com',
+setup(author = 'Hadrien Mary',
+	  author_email='hadrien.mary@gmail.com',
+	  url = 'https://github.com/hadim/read-roi/',
+	  description = 'Read ROI files .zip or .roi generated with ImageJ.',
+	  long_description = file: README.md,
+
 	  packages=find_packages(),
       classifiers=[
               'Development Status :: 5 - Production/Stable',
