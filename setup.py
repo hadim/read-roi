6cf4109f646db80cb0916496f78a0efc9329acf4
Hadrien Mary <hadrien.mary@gmail.com>
2018-11-22 19:17:48 -0500

    bumped version to 1.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='read-roi',
-      version='1.4.6',
+      version='1.5.0',
       author='Hadrien Mary',
       author_email='hadrien.mary@gmail.com',
       url='https://github.com/hadim/read-roi/',
